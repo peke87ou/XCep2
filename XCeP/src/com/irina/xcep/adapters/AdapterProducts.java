5820140ab5cfe254e80e5d7f45696567e95cf427
0010832 <0010832@PC02K218.insags.com>
2015-08-10 12:58:09 +0200

    refactor

--- XCeP/src/com/irina/xcep/adapters/AdapterProducts.java
+++ XCeP/src/com/irina/xcep/adapters/AdapterProducts.java
@@ -20,93 +20,72 @@ import com.parse.ParseException;
 import com.squareup.picasso.Picasso;
 
 public class AdapterProducts extends ArrayAdapter<Produto> {
-	
+
 	private static Map<String, Bitmap> mImagenes = new HashMap<String, Bitmap>();
-    
+
 	public AdapterProducts(Context context, ArrayList<Produto> productos) {
-       super(context, 0, productos);
-    }
+		super(context, 0, productos);
+	}
 
-    @Override
-    public View getView(int position, View celdaView, ViewGroup parent) {
-       //Recuperar o elemento de datos para esta posición
-       final Produto productosList = getItem(position);    
-              
-       // Comproba se unha vista existente está a ser reutilizado , se non inflar a vista
-       if (celdaView == null) {
-          celdaView = LayoutInflater.from(getContext()).inflate(R.layout.item_product_shopping_list, parent, false);
-       }
-       
-       //Buscar Vista para recheo de datos
-       Log.i("Productos", productosList+"");
-       ((TextView) celdaView.findViewById(R.id.name_product)).setText(productosList.getNome());
+	@Override
+	public View getView(int position, View celdaView, ViewGroup parent) {
+		// Recuperar o elemento de datos para esta posición
+		final Produto productosList = getItem(position);
 
-       ImageView imageView = (ImageView)celdaView.findViewById(R.id.image_product);
-       
+		// Comproba se unha vista existente está a ser reutilizado , se non
+		// inflar a vista
+		if (celdaView == null) {
+			celdaView = LayoutInflater.from(getContext()).inflate(
+					R.layout.item_product_shopping_list, parent, false);
+		}
 
-     //PRICE
-           try {
-        	 String precioProducto = productosList.getPrezoPorSupermercado().getQuery().getFirst().getPrice() + "";
-        	 
-        	 Log.e("NumeroEncontrado", precioProducto);
-        	 
-	     ((TextView) celdaView.findViewById(R.id.price_product)).setText(precioProducto);
-	    
-	     } catch (ParseException e) {
-	     ((TextView) celdaView.findViewById(R.id.price_product))
-	     .setText("ERROR");
-	     e.printStackTrace();
-	     }
-     
-     String unidadesProducto =  "";
-	try {
-		unidadesProducto = productosList.getPrezoPorSupermercado().getQuery().getFirst().getUnits().getQuery().getFirst().getNumberProduct() + "";
-	} catch (ParseException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}    
-     ((TextView) celdaView.findViewById(R.id.products_list)).setText(unidadesProducto);
-   
-   Bitmap bmp = mImagenes.get(productosList.getIcon());
-     
-   if(bmp != null){
-	   imageView.setImageBitmap(bmp);
-   }else{
-	   Log.d("Imagen producto: ", productosList.getIcon().getUrl());
-	   Picasso.with(getContext()).load(productosList.getIcon().getUrl()).into(imageView);
-   }
-       
-       
-      
-//       ParseRelation<ParseObject> relation = productosList.getRelation("idMarket");
-       
-//       final String objectId = productosList.getObjectId();
-     // Bitmap bmp = mImagenes.get(productosList.getUrlImaxe());
-       
-//       if(bmp != null){
-//    	   imageView.setImageBitmap(bmp);
-//       }else{
-//    	   downloadBitmap(relation, imageView);
-//       }
-//       int i = 0;
-       //Buscar Vista para recheo de datos
-//        try {
-//			 i = productosList.getIdProducts().getQuery().count();
-//		} catch (ParseException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//        String productos = "";
-//        if(i == 1){
-//        	productos = i + " Producto";
-//        }else{
-//        	productos = i + " Productos";
-//        }
-        	
-//		((TextView) celdaView.findViewById(R.id.products_list)).setText(productos);
-	
-       
-       return celdaView;
-   }
-    
+		// Buscar Vista para recheo de datos
+		Log.i("Productos", productosList + "");
+		((TextView) celdaView.findViewById(R.id.name_product))
+				.setText(productosList.getNome());
+
+		ImageView imageView = (ImageView) celdaView
+				.findViewById(R.id.image_product);
+
+		try {
+			String precioProducto = productosList.getPrezoPorSupermercado()
+					.getQuery().getFirst().getPrice()
+					+ "";
+
+			Log.e("NumeroEncontrado", precioProducto);
+
+			((TextView) celdaView.findViewById(R.id.price_product))
+					.setText(precioProducto);
+
+		} catch (ParseException e) {
+			((TextView) celdaView.findViewById(R.id.price_product))
+					.setText("ERROR");
+			e.printStackTrace();
+		}
+
+		String unidadesProducto = "";
+		try {
+			unidadesProducto = productosList.getPrezoPorSupermercado()
+					.getQuery().getFirst().getUnits().getQuery().getFirst()
+					.getNumberProduct()
+					+ "";
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		((TextView) celdaView.findViewById(R.id.products_list))
+				.setText(unidadesProducto);
+
+		Bitmap bmp = mImagenes.get(productosList.getIcon());
+
+		if (bmp != null) {
+			imageView.setImageBitmap(bmp);
+		} else {
+			Log.d("Imagen producto: ", productosList.getIcon().getUrl());
+			Picasso.with(getContext()).load(productosList.getIcon().getUrl())
+					.into(imageView);
+		}
+
+		return celdaView;
+	}
+
 }
