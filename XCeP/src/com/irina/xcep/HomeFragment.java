5c84034c828c76de9a22ffc26eefa1f0f3ebf843
0011530 <0011530@PB2T9GW.insags.com>
2015-09-02 12:35:56 +0200

    Formateo

--- XCeP/src/com/irina/xcep/HomeFragment.java
+++ XCeP/src/com/irina/xcep/HomeFragment.java
@@ -37,7 +37,7 @@ import com.parse.SaveCallback;
 
 @SuppressLint("InflateParams")
 public class HomeFragment extends Fragment {
-	
+
 	// Declaración de variables
 	public static final String TAG = HomeFragment.class.getName();
 	ButtonRectangle logout;
@@ -47,40 +47,36 @@ public class HomeFragment extends Fragment {
 	ImageButton addlist;
 	// Solicitar usuario actual do Parse.com
 	ParseUser currentUser = ParseUser.getCurrentUser();
-	
+
 	String objectIdLista = "";
-	String nameListtxt= "";
-	
-	
-	
-	public static HomeFragment newInstance (int Index){
-		
+	String nameListtxt = "";
+
+	public static HomeFragment newInstance(int Index) {
+
 		Log.d(TAG, "HomeFragment newInstance");
-		
+
 		HomeFragment fragment = new HomeFragment();
 		Bundle args = new Bundle();
-		
+
 		args.putInt("Index", Index);
-		
+
 		fragment.setArguments(args);
-		
+
 		return fragment;
 	}
-	
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		
-		
+
 		Log.d(TAG, "OncreateView");
 		RelativeLayout home = (RelativeLayout) inflater.inflate(R.layout.fragment_home, container, false);
-		
+
 		// Convertir currentUser en String
 		String struser = currentUser.getUsername().toString();
 		TextView txtuser = (TextView) home.findViewById(R.id.txtuser);
-		txtuser.setText(this.getString(R.string.text_login_home_user )+ " "  + struser);
-		
-		//Botón desconectarse da app
+		txtuser.setText(this.getString(R.string.text_login_home_user) + " " + struser);
+
+		// Botón desconectarse da app
 		logout = (ButtonRectangle) home.findViewById(R.id.logout);
 		logout.setOnClickListener(new OnClickListener() {
 			public void onClick(View arg0) {
@@ -89,54 +85,52 @@ public class HomeFragment extends Fragment {
 				getActivity().finish();
 			}
 		});
-		
-		//Botón engadir nova lista
+
+		// Botón engadir nova lista
 		addlist = (ImageButton) home.findViewById(R.id.add_list);
 		addlist.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				//Ir a páxina engadir unha lista
+				// Ir a páxina engadir unha lista
 				Intent intent = new Intent(getActivity(), AddShoppingListActivity.class);
 				startActivity(intent);
-				
+
 			}
 		});
-		
-		//Listas da compra
+
+		// Listas da compra
 		listasListView = (ListView) home.findViewById(R.id.lista_list);
 		adapter = new AdapterListas(getActivity(), misListas);
-		//Click proglongado para a modificación dunha lista
+		// Click proglongado para a modificación dunha lista
 		listasListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
-            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, final int pos, long id) {
-            	objectIdLista = misListas.get(pos).getObjectId();
-            	showDialogoModificarLista();
-            	return true;
-            }
-        }); 
-		
-		
-		//Click para acceder o detalle da lista
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1, final int pos, long id) {
+				objectIdLista = misListas.get(pos).getObjectId();
+				showDialogoModificarLista();
+				return true;
+			}
+		});
+
+		// Click para acceder o detalle da lista
 		listasListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-	        @Override
-	        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-	        	((MenuActivity)getActivity()).mNameList = misListas.get(position).getName();
-				((MenuActivity)getActivity()).mListSelected = misListas.get(position);
-	        	((MenuActivity)getActivity()).loadFragment(FragmentIndexes.FRAGMENT_LIST);
-	        }
-	    });
-		
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				((MenuActivity) getActivity()).mNameList = misListas.get(position).getName();
+				((MenuActivity) getActivity()).mListSelected = misListas.get(position);
+				((MenuActivity) getActivity()).loadFragment(FragmentIndexes.FRAGMENT_LIST);
+			}
+		});
+
 		return home;
 	}
-	
-	public void reloadUserShoppingLists() {
-		//Recreamos o conxunto de listas de compra do usuario
-		listasListView.setAdapter(adapter);
-		Log.d(TAG, "reloadUserShoppingLists"); 
 
-		final ProgressDialog progress = Utils.crearDialogoEspera(getActivity(),
-				"Actualizando listas");
+	public void reloadUserShoppingLists() {
+		// Recreamos o conxunto de listas de compra do usuario
+		listasListView.setAdapter(adapter);
+		Log.d(TAG, "reloadUserShoppingLists");
+
+		final ProgressDialog progress = Utils.crearDialogoEspera(getActivity(), "Actualizando listas");
 		progress.show();
 		ParseQuery<Lista> query = ParseQuery.getQuery(Lista.class);
 
@@ -159,8 +153,7 @@ public class HomeFragment extends Fragment {
 			@Override
 			public void done(List<Lista> objects, ParseException e) {
 				if (e != null) {
-					Toast.makeText(getActivity(), "Error na actualización de listas",
-							Toast.LENGTH_SHORT).show();
+					Toast.makeText(getActivity(), "Error na actualización de listas", Toast.LENGTH_SHORT).show();
 				}
 
 				misListas = (ArrayList<Lista>) objects;
@@ -176,132 +169,127 @@ public class HomeFragment extends Fragment {
 		});
 
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
-		//reloadUserShoppingLists();
+		// reloadUserShoppingLists();
 	}
-	
-	
-	
+
 	@Override
 	public void onStart() {
 		super.onStart();
 		reloadUserShoppingLists();
 	}
 
+	public void showDialogoModificarLista() {
 
-	public void showDialogoModificarLista(){
-		
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		builder.setTitle("Modificacións da lista" );
+		builder.setTitle("Modificacións da lista");
 		builder.setMessage("¿Que desexa facer? ");
 		builder.setPositiveButton("Eliminar a lista", new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {
-		        	   		Utils.hideSoftKeyboard(getActivity());
-		        	   		final ProgressDialog progress = Utils.crearDialogoEspera(getActivity(), "Eliminando lista");
-		        	   		progress.show();
-		        	   		
-		        		    ParseQuery<Lista> query=ParseQuery.getQuery(Lista.class);
-		        		    query.whereEqualTo("objectId",objectIdLista);
-		        		    query.findInBackground(new FindCallback<Lista>() {
-		        		        @Override
-		        		        public void done(List<Lista> parseObjects, ParseException e) {
-		        		            if(e==null) {
+			public void onClick(DialogInterface dialog, int id) {
+				Utils.hideSoftKeyboard(getActivity());
+				final ProgressDialog progress = Utils.crearDialogoEspera(getActivity(), "Eliminando lista");
+				progress.show();
 
-		        		                for (ParseObject delete : parseObjects) {
-		        		                    delete.deleteInBackground();
-		        		                }
-		        		                
-		        		                progress.dismiss();
-		        		                reloadUserShoppingLists();
-		        		            }else{
-		        		            	progress.dismiss();
-		        		                Toast.makeText(getActivity(), "Error no borrado", Toast.LENGTH_SHORT).show();
-		        		            }
-		        		        }
-		        		    });
-		        	}
-		       });
+				ParseQuery<Lista> query = ParseQuery.getQuery(Lista.class);
+				query.whereEqualTo("objectId", objectIdLista);
+				query.findInBackground(new FindCallback<Lista>() {
+					@Override
+					public void done(List<Lista> parseObjects, ParseException e) {
+						if (e == null) {
+
+							for (ParseObject delete : parseObjects) {
+								delete.deleteInBackground();
+							}
+
+							progress.dismiss();
+							reloadUserShoppingLists();
+						} else {
+							progress.dismiss();
+							Toast.makeText(getActivity(), "Error no borrado", Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+			}
+		});
 		builder.setNeutralButton("Cambiar Nome", new DialogInterface.OnClickListener() {
-			
+
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				AlertDialog.Builder popDialog = new AlertDialog.Builder(getActivity());
 				final LayoutInflater inflater = getActivity().getLayoutInflater();
-				
+
 				popDialog.setTitle("Cambiar o nome da lista ");
 				popDialog.setView(inflater.inflate(R.layout.activity_dialog_change_name_list, null))
-				.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-				    @Override
-				    public void onClick(DialogInterface dialog, int id) {
-				              
-		            	   Utils.hideSoftKeyboard(getActivity());
-		            	   
-		            	   EditText newNameList = (EditText) ((AlertDialog) dialog).findViewById(R.id.NameListNew);
-		                   final String nameListtxt = newNameList.getText().toString();
-		                   
-		                   if((nameListtxt != null) && (nameListtxt.length() > 0)){
-		                	   
-		                	   final ProgressDialog progress = Utils.crearDialogoEspera(getActivity(), "Cambiando nome");
-			            	   progress.show();
-			                   ParseQuery<Lista> query=ParseQuery.getQuery(Lista.class);
-			        		   query.whereEqualTo("objectId",objectIdLista);
-			        		   query.findInBackground(new FindCallback<Lista>() {
-			        		   @Override
-			        		   public void done(List<Lista> parseObjects, ParseException e) {
-			        			   
-			        			   if(e!= null){
-			        				   progress.dismiss();
-			        				   Toast.makeText(getActivity(), "Produciuse un erro: "+e.getMessage(), Toast.LENGTH_SHORT).show();
-			        				   return;
-			        			   }
-			        			   
-			        		       if(parseObjects.size()==1)	{
-			        		            		parseObjects.get(0).setName(nameListtxt);
-			        		            		parseObjects.get(0).saveInBackground(new SaveCallback() {
-													
+						.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int id) {
+
+								Utils.hideSoftKeyboard(getActivity());
+
+								EditText newNameList = (EditText) ((AlertDialog) dialog).findViewById(R.id.NameListNew);
+								final String nameListtxt = newNameList.getText().toString();
+
+								if ((nameListtxt != null) && (nameListtxt.length() > 0)) {
+
+									final ProgressDialog progress = Utils.crearDialogoEspera(getActivity(), "Cambiando nome");
+									progress.show();
+									ParseQuery<Lista> query = ParseQuery.getQuery(Lista.class);
+									query.whereEqualTo("objectId", objectIdLista);
+									query.findInBackground(new FindCallback<Lista>() {
+										@Override
+										public void done(List<Lista> parseObjects, ParseException e) {
+
+											if (e != null) {
+												progress.dismiss();
+												Toast.makeText(getActivity(), "Produciuse un erro: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+												return;
+											}
+
+											if (parseObjects.size() == 1) {
+												parseObjects.get(0).setName(nameListtxt);
+												parseObjects.get(0).saveInBackground(new SaveCallback() {
+
 													@Override
 													public void done(ParseException e) {
 														progress.dismiss();
-														if(e!= null){
-															Toast.makeText(getActivity(), "Produciuse un erro: "+e.getMessage(), Toast.LENGTH_SHORT).show();
-														}else{
+														if (e != null) {
+															Toast.makeText(getActivity(), "Produciuse un erro: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+														} else {
 															reloadUserShoppingLists();
 														}
 													}
-													
+
 												});
-			        		            }    				        		           
-			        		        }
-			        		    });
-			        		   
-		                   }else{
-		                	   
-		                	   Toast.makeText(getActivity(), "É necesario encher o campo nome", Toast.LENGTH_SHORT).show();
-		                   }
-				                   
-				                   
-				        }
-				   })
-				  .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-				       public void onClick(DialogInterface dialog, int id) {
-				              dialog.cancel();
-				       }
-				  });      
-				
-				 popDialog.create();
-				 popDialog.show(); 
-				            	
-			}	
+											}
+										}
+									});
+
+								} else {
+
+									Toast.makeText(getActivity(), "É necesario encher o campo nome", Toast.LENGTH_SHORT).show();
+								}
+
+							}
+						}).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
+
+				popDialog.create();
+				popDialog.show();
+
+			}
 		});
-				   
+
 		builder.setNegativeButton("Pechar", new DialogInterface.OnClickListener() {
-		           public void onClick(DialogInterface dialog, int id) {
-		        	 dialog.dismiss();
-		           }
-		       });
+			public void onClick(DialogInterface dialog, int id) {
+				dialog.dismiss();
+			}
+		});
 		AlertDialog dialogo = builder.create();
 		dialogo.show();
 	}
