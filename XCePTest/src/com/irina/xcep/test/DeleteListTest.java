efa21870039e898874b519bb9b44f43b7d8eed65
0010832 <0010832@PC02K218.insags.com>
2015-09-21 10:57:01 +0200

    TEST borrar lista

--- XCePTest/src/com/irina/xcep/test/DeleteListTest.java
+++ XCePTest/src/com/irina/xcep/test/DeleteListTest.java
@@ -1,10 +1,57 @@
 package com.irina.xcep.test;
 
-public class DeleteListTest {
+import android.test.ActivityInstrumentationTestCase2;
 
+import com.irina.xcep.SplashActivity;
+import com.robotium.solo.Solo;
+
+
+/**
+ * 1.	testDeleteListOk:  Para esta proba comprobamos que o usuario pode acceder a eliminación da lista que premeu, facendo clic na opción “Eliminar a lista”, é o test é correcto. 
+ */
+
+public class DeleteListTest extends ActivityInstrumentationTestCase2<SplashActivity> {
 	
-	/*
-	 * 1.	testDeleteListOk:  Para esta proba comprobamos que o usuario pode acceder a eliminación da lista que premeu, facendo clic na opción “Eliminar a lista”, é o test é correcto. 
-	 */
+	private Solo solo;
+	TestHelper helper;
+	static int TIME_OUT_LOGIN = 30000;
+
+	public DeleteListTest(Class<SplashActivity> activityClass) {
+		super(activityClass);
+	}
+	
+	public DeleteListTest() {
+		this(SplashActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		solo = new Solo(getInstrumentation());
+		getActivity();
+		
+		helper = new TestHelper(solo);
+	}
+	
+	@Override
+    protected void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+	
+	public void testDeleteListOk(){
+	
+		solo.waitForActivity(com.irina.xcep.SplashActivity.class, 2000);
+		assertTrue("com.irina.xcep.MenuActivity is not found!", solo.waitForActivity(com.irina.xcep.MenuActivity.class));
+		solo.waitForDialogToClose();
+		
+		
+		solo.clickLongInList(0);
+		solo.waitForDialogToOpen();
+		solo.clickOnText("Eliminar la lista");
+		
+		solo.waitForDialogToClose();
+		solo.waitForDialogToClose();
+		solo.sleep(4000);
+	}
 	 
 }
